e179cb61b4db64200993bb745949c61d920d8728
SadouDiop <thiernosadoudiop76@gmail.com>
2026-05-07 04:43:58 +0000

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,11 +1,14 @@
 const https = require('https');
-const soap = require('soap');
 
 const DL_USERNAME = 'PT296867497';
 const DL_PASSWORD = '3ntM09lv1St4';
-const DL_WSDL = 'https://store.dreamlove.es/webservices/orderservice_wsdl.php';
+const DL_FEED_XML = 'https://store.dreamlove.es/dyndata/exportaciones/csvzip/catalog_1_50_125_2_eb10a792c0336bc695e2b0ec29d88402_xml_plain.xml';
+const DL_FEED_CSV = 'https://store.dreamlove.es/dyndata/exportaciones/csvzip/catalog_1_50_125_2_eb10a792c0336bc695e2b0ec29d88402_csv_plain.csv';
 const SHOPIFY_STORE = 'salvacasal.myshopify.com';
 const SHOPIFY_CLIENT_ID = '25f03eeef4e55773fea142bea54f22b5';
 const SHOPIFY_CLIENT_SECRET = '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';
 
 console.log('Sync Dreamlove → Shopify démarré');
+console.log('Feed XML:', DL_FEED_XML);
+console.log('Feed CSV:', DL_FEED_CSV);
+console.log('Boutique:', SHOPIFY_STORE);
